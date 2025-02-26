7cb046509ed9520d3aab85500da0ff48bdc577dc
Mykhailo Petrov <mixeys2008@gmail.com>
2025-02-26 01:16:14 +0100

    Create blog page and server

---
+++ src/routes/blog/[id]/+page.server.ts
@@ -0,0 +1,18 @@
+import { error } from '@sveltejs/kit';
+
+const getPost = async (
+	id: string,
+	fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>
+) => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+	if (!res.ok) {
+		error(res.status, res.statusText);
+	}
+
+	return await res.json();
+};
+
+export async function load({ params, fetch }) {
+	return { post: getPost(params.id, fetch) };
+}
---
+++ src/routes/blog/[id]/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	const { data } = $props();
+</script>
+
+<svelte:head>
+	{#await data.post then value}
+		<title>{value.title} | My Blog</title>
+	{:catch error}
+		<title>Error | My Blog</title>
+	{/await}
+</svelte:head>
+
+<div class="p-4">
+	<a href="/blog">Back to Blog</a>
+
+	{#await data.post}
+		<p>loading...</p>
+	{:then post}
+		<h1 class="mb-4 text-center text-2xl font-bold">{post.title}</h1>
+		<p>{post.body}</p>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+</div>
